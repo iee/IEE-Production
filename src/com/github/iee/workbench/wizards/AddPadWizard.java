060f09870d8dde73ad8abc3a8a88cea4ea6e82ca
Khavrenko Sergey <khavrenkosergey@gmail.com>
2011-06-15 00:34:46 +0400

    Unimplemented newWizard extension was added

---
+++ src/com/github/iee/workbench/wizards/AddPadWizard.java
@@ -0,0 +1,22 @@
+package com.github.iee.workbench.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class AddPadWizard extends Wizard implements INewWizard {
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
