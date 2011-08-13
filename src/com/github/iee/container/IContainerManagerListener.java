74acff0485d9d9e84dbc48bffd4fe4ae69e834b3
khavrenko <khavrenkosergey@gmail.com>
2011-08-13 13:02:00 +0400

    Delete code.

--- src/com/github/iee/container/IContainerManagerListener.java
+++
@@ -1,7 +0,0 @@
-package com.github.iee.container;
-
-
-public interface IContainerManagerListener {
-	public void containerCreated(ContainerManagerEvent event);
-	public void containerRemoved(ContainerManagerEvent event);	
-}
--- src/com/github/iee/pad/PadManager.java
+++
@@ -1,5 +0,0 @@
-package com.github.iee.pad;
-
-public class PadManager {
-
-}
